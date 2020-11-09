45713923aee3bdc238402df41d3f1889729c04bc
unknown <vrnakano@gmail.com>
2020-11-09 11:57:01 -0300

    spring

--- webmvc/src/test/java/com/example/demo/WebmvcApplicationTests.java
+++ webmvc/src/test/java/com/example/demo/WebmvcApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,4 +12,20 @@ class WebmvcApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	public void testSomar() {
+		System.out.println("somar");
+		int n1 = 5;
+		int n2 = 5;
+		assertEquals(n1, n2);
+	}
+	
+	@Test
+	public void testSomar2() {
+		System.out.println("somar");
+		int n1 = 5;
+		int n2 = 7;
+		assertEquals(n1, n2);
+	}
+	
 }
